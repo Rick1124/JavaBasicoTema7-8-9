11d657a5e3c162e33a5e012934c93e38f7a6f4b3
Rick1124 <38147073+Rick1124@users.noreply.github.com>
2022-08-30 20:56:39 -0500

    1.0

--- src/poo/Main.java
+++ src/poo/Main.java
@@ -1,2 +1,35 @@
-package poo;public class Main {
+package poo;
+
+public class Main {
+    public static void main(String[] args) {
+        reverse("Hello World");
+        uni();
+        bi();
+    }
+
+    public static void reverse (String text) {
+        System.out.println("Reverse function:");
+        StringBuilder input = new StringBuilder();
+        input.append(text);
+        input.reverse();
+        System.out.println(input);
+    }
+
+    public static void uni(){
+        System.out.println("\nUni function:");
+        String[] uni = {"Hi", "Hello", "Good Morning", "What's up?"};
+        for ( String element:uni ) {
+            System.out.println(element);
+        }
+    }
+
+    public static void bi(){
+        System.out.println("\nBi function:");
+        int[][] bi = { {1, 2, 3, 4}, {5, 6, 7} };
+        for (int[] ints : bi) {
+            for (int anInt : ints) {
+                System.out.println(anInt);
+            }
+        }
+    }
 }
